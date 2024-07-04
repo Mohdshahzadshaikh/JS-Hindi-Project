2e34e0e76e5f368e6fc907675085e327a64d68b3
Mohdshahzadshaikh <shahzad@punon.in>
2024-07-04 07:17:58 +0000

    fix: created variables page

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountName = "JavaScript"
+let accountEmail ="skh123@gmail.com"
+var accountPass = "12345"
+accountCity= "Azamgarh"
+
+accountEmail="shaikh123@gmail.com"
+accountPassword="6789"
+accountCity="Jairajpur"
+
+console.table([accountId,accountName,accountEmail,accountPass,accountCity])
+console.table([accountEmail,accountPassword,accountCity])
