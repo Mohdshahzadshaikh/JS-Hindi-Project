3a7dd0d7acd065ee08665eae3195f5f65e0c9a97
Mohdshahzadshaikh <shahzad@punon.in>
2024-07-04 07:34:56 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,53 @@
 console.log("Shahzad Shaikh");
+console.log("Ameer KHan");
+console.log("Sameer Shaikh");
+
+console.log("Kazim Shaikh");
+console.log("Juber Ansari");
+
+console.log("Sameer Khan");
+console.log("Rizawan Shaikh");
+console.log("Aditya Sing");
+console.log("Vikratn ");
+console.log("Ayaan ");
+console.log("Abhimmniu ");
+
+console.log("Ajiket Shaikh");
+console.log("Perice Shaikh");
+console.log("perthmesh Shaikh");
+console.log("pervin Shaikh");
+console.log("sanjay Shaikh");
+
+console.log("waqar Shaikh");
+console.log("belal Shaikh");
+console.log("adnan Shaikh");
+
+console.log("farhan Shaikh");
+console.log("Shabaz Shaikh");
+
+console.log("Shahnwaz Shaikh");
+console.log("Serfaraz Shaikh");
+
+
+console.log("Seyaz Shaikh");
+console.log("Mosaib Shaikh");
+console.log("Waqas Shaikh");
+console.log("Arish Shaikh");
+console.log("Abushama Shaikh");
+
+console.log("Salim Shaikh");
+console.log("Modasser Shaikh");
+console.log("Humza Shaikh");
+console.log("abhisek Sharma");
+console.log("Nehal Shaikh");
+console.log("Naeem Shaikh");
+
+console.log("Salim Shaikh");
+
+console.log("Fahaz Shaikh");
+console.log("Faheem Shaikh");
+
+console.log("Zeshan Shaikh");
+console.log("Abudullah Shaikh");
+console.log("Waseem Shaikh");
+console.log("Sozab Shaikh");
