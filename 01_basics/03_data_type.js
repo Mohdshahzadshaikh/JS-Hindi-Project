f19ecf3575b95ec5bb2cdd21ee2d90375f164681
Mohdshahzadshaikh <shahzad@punon.in>
2024-07-04 07:38:07 +0000

    container config done

---
+++ 01_basics/03_data_type.js
@@ -0,0 +1,27 @@
+//"use strict"; // treat all JS code as newswer version
+
+//alert (10 + 10) // we are using nodeJs Not browser
+
+let name = "JavaScript"
+let age = 24
+let isLoggedIn = false
+let state;
+
+
+
+
+// number => 2 to power 53
+// BigInt =>
+// String  => "JavaScript"
+// Boolean => true/false
+// null => standaload value
+// undefined =>
+// Symbol => unique
+
+
+// object
+
+console.log(typeof "JavaScript");  // String
+console.log(typeof age);  // Number
+console.log(typeof null);   // Object
+console.log(typeof undefined);  // undefined
